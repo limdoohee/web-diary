f1ccfdd50f0c0311ed8594cbb8b705573113c5df
limdoohee <limdoohee@gmail.com>
2023-01-10 14:09:24 +0900

    Add Ant Design

--- src/components/NewTask.js
+++ src/components/NewTask.js
@@ -1,18 +1,16 @@
-import { useEffect, useRef } from "react";
+import { useState, useEffect, useRef } from "react";
 import styled from "styled-components";
 import { useRecoilState } from "recoil";
 import { isAddTask, newTitleState } from "../recoil/atoms";
 import { CiSaveDown1, CiCircleRemove } from "react-icons/ci";
+import { DatePicker, Select, Space, TimePicker } from 'antd';
+import dayjs from 'dayjs';
+const { Option } = Select;
 
 const Task = styled.div`
   display: ${({ isAdd }) => (isAdd ? "block" : "none")}};
   border-bottom: 1px solid #ddd;
   padding: 1em;
-  input {
-    outline: 1px solid #ddd;
-    border-radius: 0.2em;
-    padding: 0.3em;
-  }
   .mgR5 {
     margin-right: 0.5em;
   }
@@ -24,6 +22,9 @@ const InsertTitle = styled.div`
   justify-content: space-between;
   align-items: center;
   input {
+    outline: 1px solid #ddd;
+    border-radius: 0.2em;
+    padding: 0.3em;
     font-size: 1.1em;
     color: rgba(0, 0, 0, 0.8);
     width: calc(100% - 60px);
@@ -32,10 +33,21 @@ const InsertTitle = styled.div`
 
 const InsertTime = styled.div``;
 
+const disabledDate = (current) => {
+  return current && current < dayjs().endOf('day');
+};
+
+const PickerWithType = ({ type, onChange }) => {
+  if (type === 'time') return <TimePicker format={"HH:mm"} onChange={onChange} />;
+  if (type === 'date') return <DatePicker onChange={onChange} disabledDate={disabledDate} />;
+  return <DatePicker picker={type} onChange={onChange} />;
+};
+
 const NewTask = ({ saveHandler }) => {
   const [isAdd, setIsAdd] = useRecoilState(isAddTask);
   const [newTitle, setNewTitle] = useRecoilState(newTitleState);
   const newTaskRef = useRef();
+  const [type, setType] = useState('time');
 
   useEffect(() => {
     newTaskRef.current.focus();
@@ -52,7 +64,7 @@ const NewTask = ({ saveHandler }) => {
   const cancelHandler = () => {
     setNewTitle("");
     setIsAdd(false);
-  };
+  };  
 
   return (
     <Task isAdd={isAdd}>
@@ -70,7 +82,15 @@ const NewTask = ({ saveHandler }) => {
           <CiCircleRemove onClick={cancelHandler} />
         </div>
       </InsertTitle>
-      <InsertTime></InsertTime>
+      <InsertTime>
+        <Space>
+          <Select value={type} onChange={setType}>
+            <Option value="time">Time</Option>
+            <Option value="date">End</Option>
+          </Select>
+          <PickerWithType type={type}/>
+        </Space>
+      </InsertTime>
     </Task>
   );
 };

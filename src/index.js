edfedbb3e92b0882211b11664dc37dc9c46aae89
limdoohee@gmail.com <limdoohee@gmail.com>
2023-02-23 00:28:22 +0900

    Add Loading Page

--- src/index.js
+++ src/index.js
@@ -4,12 +4,13 @@ import "./style/common.css";
 import App from "./containers/App";
 import { GlobalStyle } from "./style/global";
 import { RecoilRoot } from "recoil";
+import Loading from "./containers/Loading";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <RecoilRoot>
-      <React.Suspense fallback={<div>Loading...</div>}>
+      <React.Suspense fallback={<Loading />}>
         <App />
         <GlobalStyle />
       </React.Suspense>
